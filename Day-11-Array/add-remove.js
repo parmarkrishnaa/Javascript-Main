482d305d5a136c3cbbd02effdc0c240585da1821
parmarkrishnaa <parmarkrishna050611@gmail.com>
2025-07-16 19:47:50 +0530

    searching-finding

---
+++ Day-11-Array/add-remove.js
@@ -0,0 +1,23 @@
+let items = ["Ved","Chitt","Shivam","Jarir"];
+
+console.log(items)
+
+items.push("Nurul");
+
+console.log(items)
+
+items.unshift("Pratham")
+
+console.log(items)
+
+items.splice(0,3,"Krishna")
+
+console.log(items)
+
+items.pop();
+
+console.log(items)
+
+items.shift("krishna")
+
+console.log(items)
