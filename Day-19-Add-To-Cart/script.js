3f9579e1640346349c6302ef41d2f913915d7ee5
parmarkrishnaa <parmarkrishna050611@gmail.com>
2025-08-13 23:40:55 +0530

    js pending

---
+++ Day-19-Add-To-Cart/script.js
@@ -0,0 +1,26 @@
+const product = [
+    {
+        'id' : '0',
+        'image' : 'image-path/gg-1.jpg',
+        'title' : 'Z Flip Foldable Mobile',
+        'price' : '120',
+    },
+    {
+        'id' : '1',
+        'image' : 'image-path/hh-2.jpg',
+        'title' : 'Air Pods Pro',
+        'price' : '60',
+    },
+    {
+        'id' : '2',
+        'image' : 'image-path/ee-3.jpg',
+        'title' : '250D DSLR Camera',
+        'price' : '230',
+    },
+    {
+        'id' : '3',
+        'image' : 'image-path/aa-1.jpg',
+        'title' : 'Head Phones',
+        'price' : '100',
+    }
+]
