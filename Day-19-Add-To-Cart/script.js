fafa799b6653e25e541822e9ff1116a6a8eb5e55
parmarkrishnaa <parmarkrishna050611@gmail.com>
2025-08-18 10:38:53 +0530

    add

--- Day-19-Add-To-Cart/script.js
+++ Day-19-Add-To-Cart/script.js
@@ -1,26 +1,90 @@
 const product = [
     {
-        'id' : '0',
-        'image' : 'image-path/gg-1.jpg',
-        'title' : 'Z Flip Foldable Mobile',
-        'price' : '120',
+        'id': '0',
+        'image': 'image-path/gg-1.jpg',
+        'title': 'Z Flip Foldable Mobile',
+        'price': '120',
     },
     {
-        'id' : '1',
-        'image' : 'image-path/hh-2.jpg',
-        'title' : 'Air Pods Pro',
-        'price' : '60',
+        'id': '1',
+        'image': 'image-path/hh-2.jpg',
+        'title': 'Air Pods Pro',
+        'price': '60',
     },
     {
-        'id' : '2',
-        'image' : 'image-path/ee-3.jpg',
-        'title' : '250D DSLR Camera',
-        'price' : '230',
+        'id': '2',
+        'image': 'image-path/ee-3.jpg',
+        'title': '250D DSLR Camera',
+        'price': '230',
     },
     {
-        'id' : '3',
-        'image' : 'image-path/aa-1.jpg',
-        'title' : 'Head Phones',
-        'price' : '100',
+        'id': '3',
+        'image': 'image-path/aa-1.jpg',
+        'title': 'Head Phones',
+        'price': '100',
     }
-]
+];
+
+// No need to use Set here, just use product array directly
+const categories = [...product];
+
+let i = 0;
+
+document.getElementById("root").innerHTML = categories.map((item, idx) => {
+    var { image, title, price } = item;
+    return (
+        `
+            <div class="box">
+                <div class="img-box">
+                    <img class="images" src="${image}" />
+                </div>
+                <div class="bottom">
+                    <p>${title}</p>
+                    <h2>$${price}.00</h2>
+                    <button onclick='addtocart(${idx})'>Add to cart</button>
+                </div>
+            </div>
+        `
+    );
+}).join("");
+
+var cart = [];
+
+function addtocart(a) {
+    cart.push({ ...categories[a] });
+    displaycart();
+}
+
+function delElement(a) {
+    cart.splice(a, 1);
+    displaycart();
+}
+
+function displaycart() {
+    let j = 0, total = 0;
+
+    document.getElementById("count").innerHTML = cart.length;
+
+    if (cart.length === 0) {
+        document.getElementById("cartItem").innerHTML = `Your cart is empty`;
+        document.getElementById("total").innerHTML = "$0.00";
+    } else {
+        document.getElementById("cartItem").innerHTML = cart.map((items) => {
+            var { image, title, price } = items;
+            total += Number(price);
+            return (
+                `
+                    <div class="cart-item">
+                        <div class="row-img">
+                            <img class="rowimg" src="${image}" />
+                        </div>
+                        <p style="font-size: 12px;">${title}</p>
+                        <h2 style="font-size: 15px;">$${price}.00</h2>
+                        <i class='fa-solid fa-trash' onclick='delElement(${j++})'></i>
+                    </div>
+                `
+            );
+        }).join("");
+        document.getElementById("total").innerHTML = "$" + total + ".00";
+    }
+}
